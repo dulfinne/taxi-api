8f2195dc8bd295c1f48d1a43a87bfb8aa798746f
yani <yanazhuchok@gmail.com>
2024-10-31 20:59:42 +0300

    remove: unused imports

--- passenger-service/src/main/java/com/dulfinne/taxi/passengerservice/PassengerServiceApplication.java
+++ passenger-service/src/main/java/com/dulfinne/taxi/passengerservice/PassengerServiceApplication.java
@@ -4,9 +4,6 @@ import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.time.ZoneId;
-import java.time.ZonedDateTime;
 import java.util.TimeZone;
 
 @SpringBootApplication
