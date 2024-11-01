1edd80657d52fb748c09de7b3cc1774f11aa8481
yani <yanazhuchok@gmail.com>
2024-11-01 14:41:21 +0300

    delete: Passenger classes and Converters

--- passenger-service/src/main/resources/db/changelog/v-1.0/01-create-passenger-table.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE passenger
-(
-    id         BIGSERIAL PRIMARY KEY,
-    username   VARCHAR(20) UNIQUE NOT NULL,
-    password   VARCHAR NOT NULL,
-    email      VARCHAR(255) NOT NULL,
-    created_at TIMESTAMPTZ  NOT NULL
-);
--- passenger-service/src/main/resources/db/changelog/v-1.0/01-drop-passenger-table.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE IF EXISTS passenger_info
-DROP
-CONSTRAINT IF EXISTS fk_passenger_info_id;
-
-DROP TABLE IF EXISTS passenger;
