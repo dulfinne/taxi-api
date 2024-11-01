0d3d898fb79046b9d65b8292ba6f36cb8a8a97e6
yani <yanazhuchok@gmail.com>
2024-11-01 14:47:37 +0300

    add: static mapper

---
+++ passenger-service/src/main/java/com/dulfinne/taxi/passengerservice/mapper/PassengerRatingMapper.java
@@ -0,0 +1,15 @@
+package com.dulfinne.taxi.passengerservice.mapper;
+
+import com.dulfinne.taxi.passengerservice.dto.response.PassengerRatingResponse;
+import com.dulfinne.taxi.passengerservice.model.PassengerRating;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+@Mapper
+public interface PassengerRatingMapper {
+  PassengerRatingMapper RATING_MAPPER_INSTANCE = Mappers.getMapper(PassengerRatingMapper.class);
+
+  PassengerRatingResponse toResponse(PassengerRating entity);
+
+  PassengerRating toEntity(PassengerRatingResponse response);
+}
