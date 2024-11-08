65cb1f7ed99b8b5427a8ef8ed7415cc4f82459ac
yani <yanazhuchok@gmail.com>
2024-11-08 17:04:38 +0300

    add: static method to get RideStatus from id

--- ride-service/src/main/kotlin/com/dulfinne/taxi/rideservice/model/RideStatus.kt
+++ ride-service/src/main/kotlin/com/dulfinne/taxi/rideservice/model/RideStatus.kt
@@ -1,9 +1,16 @@
 package com.dulfinne.taxi.rideservice.model
 
-enum class RideStatus(val description: String) {
-    SEARCHING("searching for driver"),
-    ACCEPTED("accepted by driver"),
-    IN_PROGRESS("ride is in progress"),
-    COMPLETED("ride is completed"),
-    CANCELLED("ride is canceled"),
+enum class RideStatus(val id: Int) {
+    SEARCHING(1),
+    ACCEPTED(2),
+    IN_PROGRESS(3),
+    COMPLETED(4),
+    CANCELLED(5);
+
+    companion object {
+        fun fromId(id: Int): RideStatus {
+            return entries.find { it.id == id }
+                ?: throw IllegalArgumentException("Invalid id for RideStatus: $id")
+        }
+    }
 }
