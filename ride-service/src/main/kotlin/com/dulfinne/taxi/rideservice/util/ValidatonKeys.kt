9cda7453de99fb20013d18522600d49290d38c31
yani <yanazhuchok@gmail.com>
2024-11-14 14:58:39 +0300

    add: new validation keys

--- ride-service/src/main/kotlin/com/dulfinne/taxi/rideservice/util/ValidatonKeys.kt
+++ ride-service/src/main/kotlin/com/dulfinne/taxi/rideservice/util/ValidatonKeys.kt
@@ -6,4 +6,6 @@ object ValidationKeys {
     const val END_POSITION_REQUIRED = "end-position-required"
     const val INVALID_RATING_RANGE = "invalid-rating-range"
     const val INVALID_FEEDBACK_LENGTH = "invalid-feedback-length"
+    const val LATITUDE_REQUIRED = "latitude-required"
+    const val LONGITUDE_REQUIRED = "longitude-required"
 }
