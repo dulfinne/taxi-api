1020014bd799f31d2fbd7a98a7164b687a345500
yani <yanazhuchok@gmail.com>
2024-11-19 12:43:11 +0300

    minor

--- passenger-service/src/main/java/com/dulfinne/taxi/passengerservice/model/PassengerRating.java
+++ passenger-service/src/main/java/com/dulfinne/taxi/passengerservice/model/PassengerRating.java
@@ -42,4 +42,28 @@ public class PassengerRating {
 
   @Column(name = "feedback")
   private String feedback;
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null) return false;
+    Class<?> oEffectiveClass =
+            o instanceof HibernateProxy
+                    ? ((HibernateProxy) o).getHibernateLazyInitializer().getPersistentClass()
+                    : o.getClass();
+    Class<?> thisEffectiveClass =
+            this instanceof HibernateProxy
+                    ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass()
+                    : this.getClass();
+    if (thisEffectiveClass != oEffectiveClass) return false;
+    PassengerRating passengerRating = (PassengerRating) o;
+    return getId() != null && Objects.equals(getId(), passengerRating.getId());
+  }
+
+  @Override
+  public int hashCode() {
+    return this instanceof HibernateProxy
+            ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass().hashCode()
+            : getClass().hashCode();
+  }
 }
