b38d89dbc812b855c9b6448e1f7505e649320649
yani <yanazhuchok@gmail.com>
2024-11-20 00:04:57 +0300

    impl: api-gateway

---
+++ api-gateway/src/test/java/com/dulfinne/taxi/apigateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dulfinne.taxi.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
