e2c0ed0cd424bedd584d75559dc1acbef5db8171
yani <yanazhuchok@gmail.com>
2024-11-19 22:37:04 +0300

    impl: eureka-server

--- passenger-service/src/main/java/com/dulfinne/taxi/passengerservice/PassengerServiceApplication.java
+++ passenger-service/src/main/java/com/dulfinne/taxi/passengerservice/PassengerServiceApplication.java
@@ -4,9 +4,12 @@ import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
 import java.util.TimeZone;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class PassengerServiceApplication {
 
     @Value("${SYSTEM_TIMEZONE}")
