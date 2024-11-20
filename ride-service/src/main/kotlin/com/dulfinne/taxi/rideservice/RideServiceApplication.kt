6fe20a5e1f5667dd1337bef8d06b202c370f8daf
dulfinne <101965340+dulfinne@users.noreply.github.com>
2024-11-20 12:39:40 +0300

    Eureka-server + Api-gateway (#15)
    * impl: eureka-server
    * impl: api-gateway

--- ride-service/src/main/kotlin/com/dulfinne/taxi/rideservice/RideServiceApplication.kt
+++ ride-service/src/main/kotlin/com/dulfinne/taxi/rideservice/RideServiceApplication.kt
@@ -4,9 +4,11 @@ import jakarta.annotation.PostConstruct
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient
 import java.util.TimeZone
 
 @SpringBootApplication
+@EnableDiscoveryClient
 class RideServiceApplication {
 
     @Value("\${SYSTEM_TIMEZONE}")
