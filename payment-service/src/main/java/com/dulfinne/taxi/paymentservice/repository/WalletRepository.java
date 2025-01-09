0e10983780651d0d2df38c6c420ed994f635e355
yani <yanazhuchok@gmail.com>
2025-01-09 15:29:12 +0300

    add: scheduled debt repayment at 00:00

--- payment-service/src/main/java/com/dulfinne/taxi/paymentservice/repository/WalletRepository.java
+++ payment-service/src/main/java/com/dulfinne/taxi/paymentservice/repository/WalletRepository.java
@@ -4,9 +4,12 @@ import com.dulfinne.taxi.paymentservice.model.Wallet;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.math.BigDecimal;
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface WalletRepository extends JpaRepository<Wallet, Long> {
   Optional<Wallet> findByUsername(String username);
+  List<Wallet> findAllByDebtGreaterThan(BigDecimal debt);
 }
--- payment-service/src/test/java/com/dulfinne/taxi/paymentservice/util/PaymentTestData.java
+++ payment-service/src/test/java/com/dulfinne/taxi/paymentservice/util/PaymentTestData.java
@@ -11,9 +11,11 @@ public class PaymentTestData {
   public static final String PASSENGER_USERNAME = "anna123";
   public static final String DRIVER_USERNAME = "zhanna123";
   public static final BigDecimal PAYMENT_AMOUNT = BigDecimal.valueOf(77);
+  public static final Long RIDE_ID = 1L;
 
   public static PaymentRequest getPaymentRequest() {
     return PaymentRequest.newBuilder()
+            .setRideId(RIDE_ID)
             .setPassengerUsername(PASSENGER_USERNAME)
             .setDriverUsername(DRIVER_USERNAME)
             .setPrice(PAYMENT_AMOUNT.toString())
